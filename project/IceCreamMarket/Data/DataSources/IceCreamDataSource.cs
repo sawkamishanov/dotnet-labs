6f6a6d24848746472aaa1c9dcbcd0443064ea55a
aamishanov <aamishanov@gmail.com>
2020-04-13 05:56:01 +0300

    fix migrations

--- project/IceCreamMarket/Data/DataSources/IceCreamDataSource.cs
+++ project/IceCreamMarket/Data/DataSources/IceCreamDataSource.cs
@@ -7,8 +7,12 @@ namespace Data.DataSources
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+
+        public IEnumerable<ActiveOrders> Orders { get; set; }
         public IEnumerable<CompositionDataSource> Compositions { get; set; }
+        [Required]
         public double Price { get; set; }
         public string Description { get; set; }
     }
